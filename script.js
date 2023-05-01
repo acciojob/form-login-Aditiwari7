5a162d297b5950fccb27d2e8552a2731f5216675
Aditi Tiwari <115400068+Aditiwari7@users.noreply.github.com>
2023-05-01 13:30:40 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,4 +1,15 @@
 function getFormvalue() {
     //Write your code here
 
+	var form = document.querySelector("form1");
+
+	form.addEventListener("submit", function(event){
+		event.preventDefault();
+
+		var fname = document.getElementById("fname").value;
+		var lname = document.getElementById("lname").value;
+
+		alert("First Name: " + fname + "\nLast Name :" + lname);
+	});
+
 }
